c26523b9abe979561b4531dd5bf38b0fad97cbd0
Zach Ingbretsen <zingbretsen@fas.harvard.edu>
2018-03-27 18:48:58 -0400

    Also print truncated source for each question

--- scripts/print_pos.py
+++ scripts/print_pos.py
@@ -6,7 +6,7 @@ from utils import data
 finder = data.FileFinder()
 sat = finder.get_sat_data()
 
-with open('pos.txt', 'w') as f:
-    f.write('POS1,POS2')
+with open('pos_and_src.txt', 'w') as f:
+    f.write('POS1,POS2,Source')
     for q in sat:
-        f.write('\n' + ','.join(q['question_POS']))
+        f.write('\n' + ','.join(q['question_POS'] + q['source'].split()[:1]))
